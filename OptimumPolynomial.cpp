9bcf78147412d03c20721a1ef525263c02d9df1e
cyp <cyp@hype>
2018-08-23 14:26:21 +0800

    add Opt

--- OptimumPolynomial.cpp
+++ OptimumPolynomial.cpp
@@ -5,25 +5,31 @@
 
 using namespace std;
 
-vector<float> LagrangePolynomial(int n, vector<float> xVals, vector<float> yVals);
-int f(n);
+vector<float> LagrangePolynomial(int k, int n, vector<float> xVals, vector<float> yVals);
+float f(n);
 
 int main()
 {
+    vector<float> xVals;
+    vector<float> yVals;
+
+    for (int i = 1; i < 11; i++)
+    {
+        xVals.push_back(i);
+        yVals.push_back(f(i));
+    }
+
+    int res = 0;
     for (int i = 1; i < 12; i++)
     {
-        for (int j = 1; j < i+1; j++)
-        {
-
-        }
+        res += LagrangePolynomial(1, i, xVals, yVals);
     }
+    cout << res << endl;
 }
 
-vector<float> LagrangePolynomial(int n, vector<float> xVals, vector<float> yVals)
+vector<float> LagrangePolynomial(int k, int n, vector<float> xVals, vector<float> yVals)
 {
     assert(xVals.size() == yVals.size());
-
-    int k = yVals.size();
 
     int res = 0;
     for (int i = 0; i < k; i++)
@@ -42,7 +48,7 @@ vector<float> LagrangePolynomial(int n, vector<float> xVals, vector<float> yVals
     return res;
 }
 
-int f(n)
+float f(n)
 {
     return (pow(-1*n, 10) - 1)/(-1*n - 1);
 }
