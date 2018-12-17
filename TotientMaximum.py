c34e9a12c3a7d067ceb5eb7524927c7c57bc35a1
YipingChen <chenypress@gmail.com>
2018-12-17 06:43:59 -0800

    solve 69

--- TotientMaximum.py
+++ TotientMaximum.py
@@ -1,39 +1,33 @@
 import MathUtils as mu
 
-pt = mu.genPrimeTab(1000000)
+pt = mu.genPrimeTab(1000003)
 
 def get_p(n):
-    pl = set()
-    i = 0
-    while pt[i] < n:
-        if n % pt[i] == 0:
-            pl.add(pt[i])
-            n = n // pt[i]
-            i = 0
-        else:
-            i += 1
-    if pt[i] == n:
-        pl.add(pt[i])
+    pl = []
+    for p in pt:
+        if p * p > n:
+            break
+        while n % p == 0:
+            pl.append(p)
+            n //= p
+    if n > 1:
+        pl.append(n)
     return pl
 
 def phi(n):
-    res = []
-    for i in range(1, n):
-        b = True
-        for j in get_p(n):
-            if i % j == 0:
-                b = False
-                break
-        if b:
-            res.append(i)
+    res = n
+    p = set(get_p(n))
+    for pp in p:
+        # res -= (res / pp)
+        res = res // pp * (pp - 1)
     return res
 
 max = 1
 n = 1
-for i in range(2, 50):
+for i in range(2, 1000001):
     p = phi(i)
-    if i / len(p) > n / max:
-        max = len(p)
+    if float(i) / p > float(n) / max:
+        max = p
         n = i
-    print(i, p)
-print(n, max, n / max)
+    # print(i, p)
+print(n, max, float(n) / max)
