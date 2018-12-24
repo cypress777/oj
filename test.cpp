a7b6174489e6000a884a26d5ebd2ef0dc64c0071
YipingChen <chenypress@gmail.com>
2018-12-24 08:22:24 -0800

    test

--- test.cpp
+++ test.cpp
@@ -1,31 +1,52 @@
-//
-// Created by cypress on 2018/8/26.
-//
-
-#include <iostream>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <cmath>
+#include <queue>
+#include <ctime>
+#include <bitset>
+#include <string>
+#include <cstdio>
 #include <vector>
-#include "LargeNumHelper.hpp"
-
+#include <fstream>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
 using namespace std;
 
-int main()
-{
-    vector<char> a{'1', '4', '5'};
-    vector<char> b{'6', '7', '2'};
-    vector<char> c = largeNumMult(b, b);
-    for (int i = 0; i < c.size(); i++)
-        cout << c[i] << " ";
-    cout << endl;
-    mutLargeNumSum(a, b);
-    for (int i = 0; i < b.size(); i++)
-        cout << b[i] << " ";
-    cout << endl;
-    mutLargeNumSubtraction(b, a);
-    for (int i = 0; i < a.size(); i++)
-        cout << a[i] << " ";
-    cout << endl;
-    mutLargeNumMult(a, a);
-    for (int i = 0; i < a.size(); i++)
-        cout << a[i] << " ";
-    cout << endl;
+int bfs(int idx, const vector<vector<int>> &cvts, vector<int> &alps, vector<int> &flgs) {
+    int cnt = 0;
+    for (int i = 0; i < 26; i++) {
+        if (cvts[i][idx] ==1 && flgs[i][idx] != -1) {
+            flgs[i][idx] = -1;
+            cnt += alps[i][idx] + bfs(i, cvts, alps, flgs);
+            if (i == 'h' - 'a') {
+
+            }
+        }
+    }
+}
+
+int main() {
+    int N;
+    cin >> N;
+
+    string S;
+    cin >> S;
+    vector<int> alps(26, 0);
+    for (auto s : S) {
+        alps[s - 'a'] += 1;
+    }
+
+    vector<vector<int>> convert(26, vector<int>(26, 0));
+    for (int i = 0; i < N; i++) {
+        string ss;
+        cin >> ss;
+        cout << from << ' ' << to;
+        convert[from][to] = 1;
+    }
+
+    vector<vector<int>> flgs(26, vector<int>(26, 0));
+    int res = bfs('h' - 'a', convert, alps, flgs);
 }
