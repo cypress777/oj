067ab7fab31800fbb84b7666ed73c9408b42ea1d
YipingChen <chenypress@gmail.com>
2018-12-24 08:38:02 -0800

    test

--- test.cpp
+++ test.cpp
@@ -18,11 +18,10 @@ using namespace std;
 int bfs(int idx, const vector<vector<int>> &cvts, vector<int> &alps, vector<int> &flgs) {
     int cnt = 0;
     for (int i = 0; i < 26; i++) {
-        if (cvts[i][idx] ==1 && flgs[i][idx] != -1) {
-            flgs[i][idx] = -1;
-            cnt += alps[i][idx] + bfs(i, cvts, alps, flgs);
-            if (i == 'h' - 'a') {
-
+        if (flgs[i] != -1) {
+            flgs[i] = -1;
+            if (cvts[i][idx] == 1) {
+                cnt += alps[i] + bfs(i, cvts, alps, flgs);
             }
         }
     }
@@ -43,10 +42,13 @@ int main() {
     for (int i = 0; i < N; i++) {
         string ss;
         cin >> ss;
+        char from = ss[0];
+        char to = ss[2];
         cout << from << ' ' << to;
         convert[from][to] = 1;
     }
 
-    vector<vector<int>> flgs(26, vector<int>(26, 0));
-    int res = bfs('h' - 'a', convert, alps, flgs);
+    vector<int> flgs(26, 0);
+    flgs['h' - 'a'] = -1;
+    int res = alps['h' - 'a'] + bfs('h' - 'a', convert, alps, flgs);
 }
