a2a879e6f273547774b30bc9a6e797edae916da5
YipingChen <chenypress@gmail.com>
2019-01-14 08:48:05 -0800

    solve hiho o90 p4

--- hiho_o90_p4.cpp
+++ hiho_o90_p4.cpp
@@ -4,11 +4,10 @@
 
 using namespace std;
 
-int is_valid(const string &a) {
+int get_num(const string &a) {
     if (a.size() == 1 && a[0] == 'n') return -1;
     int num = 0;
     for (int i = 0; i < a.size(); i++) {
-        if (a[i] - '0' < 0 || a[i] - '9' > 0) return -2;
         num *= 10;
         num += a[i] - '0';
     }
@@ -19,9 +18,9 @@ int main() {
     int N;
     cin >> N;
 
-    unordered_set<char> params_set;
-    vector<int> loop_times;
-    vector<char> param_que;
+    unordered_set<string> params_set;
+    vector<int> loop_times(1, 0);
+    vector<string> param_que;
 
     int complexity = 0;
     int flag = 1;
@@ -33,27 +32,32 @@ int main() {
             string arg, x, y;
             cin >> arg >> x >> y;
 
-            if (arg.size() != 1) continue;
-
-            int xx = is_valid(x);
-            int yy = is_valid(y);
-            if (params_set.find(arg[0]) != params_set.end() || xx == -2 || yy == -2) {
+            int xx = get_num(x);
+            int yy = get_num(y);
+            if (params_set.find(arg) != params_set.end()) {
                 flag = -1;
                 break;
             } else {
-                params_set.insert(arg[0]);
-                param_que.push_back(arg[0]);
-                if (xx == -1 && yy == -1 || yy != -1 && xx != -1 && yy - xx >= 0) {
-                    loop_times.push_back(0);
+                int cur_dep = param_que.size();
+                params_set.insert(arg);
+                param_que.push_back(arg);
+                if (loop_times[cur_dep] == -1) {
+                    loop_times.push_back(-1);
+                } else if (xx == -1 && yy == -1 || yy != -1 && xx != -1 && yy - xx >= 0) {
+                    loop_times.push_back(loop_times[cur_dep]);
                 } else if (xx == -1 && yy != -1 || yy != -1 && xx != -1 && xx - yy > 0) {
                     loop_times.push_back(-1);
                 } else if (yy == -1) {
-                    loop_times.push_back(1);
+                    loop_times.push_back(loop_times[cur_dep] + 1);
                 }
             }
-            cout << state[0] << " " << arg[0] << " " << x << " " << y << endl;
+            // cout << state[0] << " " << arg[0] << " " << x << " " << y << endl;
+            // for (int i = 0; i < loop_times.size(); i++) {
+            //     cout << loop_times[i] << " ";
+            // }
+            // cout << endl;
         } else if (state[0] == 'E') {
-            cout << state[0] << endl;
+            // cout << state[0] << endl;
             int size = param_que.size();
             if (size == 0) {
                 flag = -1;
@@ -61,24 +65,29 @@ int main() {
             } else {
                 params_set.erase(param_que[size - 1]);
                 param_que.pop_back();
-                int pow  = 0;
-                for (int k = 0; k < loop_times.size(); k++) {
-                    cout << loop_times[k] << endl;
-                }
-                for (int k = 0; k < loop_times.size(); k++) {
-                    if (loop_times[k] == -1) break;
-                    pow += loop_times[k];
-                }
-                loop_times = vector<int>{};
-                complexity = complexity > pow ? complexity : pow;
+                complexity = complexity > loop_times[size] ? complexity : loop_times[size];
+                loop_times.pop_back();
             }
+            // for (int i = 0; i < loop_times.size(); i++) {
+            //     cout << loop_times[i] << " ";
+            // }
+            // cout << endl;
         }
     }
+
+    if (param_que.size() != 0) flag = -1;
 
     if (flag == -1) {
         cout << "CE" << endl;
     } else {
-        cout << "O(n^" << to_string(complexity) << ")" << endl;
+        if (complexity == -1) complexity = 0;
+        if (complexity == 0) {
+            cout << "O(1)" << endl;
+        } else if (complexity == 1) {
+            cout << "O(n)" << endl;
+        } else {
+            cout << "O(n^" << to_string(complexity) << ")" << endl;
+        }
     }
 
     return 0;
