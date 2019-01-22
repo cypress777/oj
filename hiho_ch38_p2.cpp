aa88a0aef8007e330b19b38ef75e61071079c828
YipingChen <chenypress@gmail.com>
2019-01-22 08:05:00 -0800

    solve hiho ch38 p2

--- hiho_ch38_p2.cpp
+++ hiho_ch38_p2.cpp
@@ -16,35 +16,45 @@ int main() {
         cin >> seq[i];
     }
 
-    // fs[i][j]: amount of number se than j in seq[1 : i]
-    // fl[i][j]: amount of number le than j in seq[1 : i]
-    // bs[i][j]: amount of number se than j in seq[i : n]
-    // bl[i][j]: amount of number le than j in seq[i : n]
-
     fs = vector<vector<int>>(n + 2, vector<int>(n + 2, 0));
     fl = vector<vector<int>>(n + 2, vector<int>(n + 2, 0));
     bs = vector<vector<int>>(n + 2, vector<int>(n + 2, 0));
     bl = vector<vector<int>>(n + 2, vector<int>(n + 2, 0));
 
     for (int i = 1; i <= n; i++) {
-        for (int j = 0; j <= n + 1; j++) fs[i][j] = fs[i - 1][j] + seq[i] <= j ? 1 : 0;
-        for (int j = 0; j <= n + 1; j++) fl[i][j] = fl[i - 1][j] + seq[i] >= j ? 1 : 0;
+        for (int j = 1; j <= n; j++) {
+            fs[i][j] = fs[i - 1][j];
+            fl[i][j] = fl[i - 1][j];
+        }
+        for (int j = seq[i]; j <= n; j++) fs[i][j] = max(fs[i][j], fs[i - 1][seq[i] - 1] + 1);
+        for (int j = seq[i]; j >= 1; j--) fl[i][j] = max(fl[i][j], fl[i - 1][seq[i] + 1] + 1);
     }
     for (int i = n; i >= 1; i--) {
-        for (int j = 0; j <= n + 1; j++) bs[i][j] = bs[i + 1][j] + seq[i] <= j ? 1 : 0;
-        for (int j = 0; j <= n + 1; j++) bl[i][j] = bl[i + 1][j] + seq[i] >= j ? 1 : 0;
+        for (int j = 1; j <= n; j++) {
+            bs[i][j] = bs[i + 1][j];
+            bl[i][j] = bl[i + 1][j];
+        }
+        for (int j = seq[i]; j <= n; j++) bs[i][j] = max(bs[i][j], bs[i + 1][seq[i] - 1] + 1);
+        for (int j = seq[i]; j >= 1; j--) bl[i][j] = max(bl[i][j], bl[i + 1][seq[i] + 1] + 1);
     }
 
+    // for (int i = 0; i <= n + 1; i++) {
+    //     for (int j = 0; j <= n + 1; j++) {
+    //         cout << "fs: " << i << " " << j << " " << fs[i][j] << endl;
+    //         cout << "fl: " << i << " " << j << " " << fl[i][j] << endl;
+    //         cout << "bs: " << i << " " << j << " " << bs[i][j] << endl;
+    //         cout << "bl: " << i << " " << j << " " << bl[i][j] << endl;
+    //     }
+    // }
+    // return 0;
+
     int res = 0;
-    for (int i = 0; i <= n; i++) {
-        for (int j = 0; j <= n + 1; j++) {
+    for (int i = 1; i < n; i++) {
+        for (int j = 1; j < n; j++) {
             res = max(res, fs[i][j] + fl[i][j + 1] + bs[i + 1][j] + bl[i + 1][j + 1]);
-            cout << "-----------" << i << " " << j << " " << res << endl;
-            cout << fs[i][j] << " " << fl[i][j + 1] << endl;
-            cout << bs[i + 1][j] << " " << bl[i + 1][j + 1] << endl;
         }
     }
     cout << res << endl;
-
+ 
     return 0;
 }
