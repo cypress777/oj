be3226dbacbf2975b68982110491ed72baca99b3
heng.pan <heng.pan@hypereal.com>
2019-02-20 18:34:42 +0800

    solving 98

---
+++ AnagramicSquares.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+bool is_valid(int n, const vector<int> &aidx, const vector<int> &bidx) {
+    vector<int> nn(aidx.size(), 0);
+    for (int i = aidx.size() - 1; i >= 0; i--) {
+        nn[i] = n % 10;
+        n /= 10;
+    }
+    if (nn[0] == 0) return false;
+
+    return true;
+}
+
+bool is_pair(const string &sa, const string &sb, vector<int> &aidx, vector<int> &bidx) {
+    if (sa.size() != sb.size()) return false;
+
+    aidx = vector<int>(sa.size(), -1);
+    bidx = vector<int>(sa.size(), -1);
+    vector<int> flg(sa.size(), -1);
+
+    for (int i = 0; i < sa.size(); i++) {
+        if (aidx[i] == -1) {
+            aidx[i] = i;
+            for (int j = i + 1; j < sa.size(); j++) {
+                if (aidx[j] != -1) continue;
+                if (sa[j] == sa[i]) aidx[j] = aidx[i];
+            }
+        }
+
+        bool has_a = false;
+        for (int j = 0; j < sa.size(); j++) {
+            if (flgs[j] == -1 && sb[j] == sa[i]) {
+                flgs[j] = 1;
+                has_a = true;
+                bidx[i] = aidx[i];
+                break;
+            }
+        }
+        if (!has_a) return false;
+    }
+    return true;
+}
+
+int cal(const vector<int> &aidx, const vector<int> &bidx) {
+    int digits = aidx.size();
+    int max_range = pow(10, digits + 1);
+    int min_range = pow(10, digits);
+    for (int i = static_cast<int>(sqrt(max_range)); i > static_cast<int>(sqrt(min_range)); i--) {
+        int n = i * i;
+        if (is_valid(n, aidx, bidx)) return n;
+    }
+    return -1;
+}
+
+int main() {
+    ifstream fin("p098_words.txt", ifstream::in);
+    vector<string> words;
+
+    while (fin.good()) {
+        string s;
+        fin >> s;
+        words.push_back(s);
+    }
+
+    int max_sq = 0;
+    string max_a;
+    string max_b;
+    for (int i = 0; i < words.size(); i++) {
+        string sa = words[i];
+        for (int j = i + 1; j < words.size(); j++) {
+            string sb = words[j];
+            vector<int> aidx, bidx;
+            if (!is_pair(sa, sb, aidx, bidx)) continue;
+
+            int sq = cal(aidx, bidx);
+            if (sq > max_sq) max_sq = sq, max_a = sa, max_b = sb;
+        }
+    }
+
+    cout << max_sq << " " << max_a << " " << max_b << endl;
+
+    return 0;
+}
