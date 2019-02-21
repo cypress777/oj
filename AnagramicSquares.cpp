b8bb8abe44ab5aad430efc28378f38e0deebf589
heng.pan <heng.pan@hypereal.com>
2019-02-21 16:44:15 +0800

    solve 98

--- AnagramicSquares.cpp
+++ AnagramicSquares.cpp
@@ -7,13 +7,30 @@
 using namespace std;
 
 bool is_valid(int n, const vector<int> &aidx, const vector<int> &bidx) {
-    vector<int> nn(aidx.size(), 0);
-    for (int i = aidx.size() - 1; i >= 0; i--) {
-        nn[i] = n % 10;
-        n /= 10;
+    vector<int> flg(aidx.size(), -1);
+    int bn = 0;
+    // n for a, formulate bn
+    int base_i = static_cast<int>(pow(10, aidx.size()));
+    for (int i = 0; i < aidx.size(); i++) {
+        base_i /= 10;
+        int ni = n / base_i % 10;
+        int base_j = base_i;
+        for (int j = i + 1; j < aidx.size(); j++) {
+            base_j /= 10;
+            int nj = n / base_j % 10;
+            if (aidx[j] == aidx[i] && ni != nj) return false;
+            if (aidx[j] != aidx[i] && ni == nj) return false;
+        }
+        for (int j = 0; j < aidx.size(); j++) {
+            if (flg[j] == -1 && bidx[j] == aidx[i]) {
+                flg[j] = 1;
+                if (j == 0 && ni == 0) return false;
+                bn += ni * static_cast<int>(pow(10, aidx.size() - j - 1));
+            }
+        }
     }
-    if (nn[0] == 0) return false;
-
+    int sqb = static_cast<int>(sqrt(bn));
+    if (sqb * sqb != bn) return false;
     return true;
 }
 
@@ -35,10 +52,10 @@ bool is_pair(const string &sa, const string &sb, vector<int> &aidx, vector<int> 
 
         bool has_a = false;
         for (int j = 0; j < sa.size(); j++) {
-            if (flgs[j] == -1 && sb[j] == sa[i]) {
-                flgs[j] = 1;
+            if (flg[j] == -1 && sb[j] == sa[i]) {
+                flg[j] = 1;
                 has_a = true;
-                bidx[i] = aidx[i];
+                bidx[j] = aidx[i];
                 break;
             }
         }
@@ -49,11 +66,11 @@ bool is_pair(const string &sa, const string &sb, vector<int> &aidx, vector<int> 
 
 int cal(const vector<int> &aidx, const vector<int> &bidx) {
     int digits = aidx.size();
-    int max_range = pow(10, digits + 1);
-    int min_range = pow(10, digits);
+    int max_range = pow(10, digits);
+    int min_range = pow(10, digits - 1);
     for (int i = static_cast<int>(sqrt(max_range)); i > static_cast<int>(sqrt(min_range)); i--) {
         int n = i * i;
-        if (is_valid(n, aidx, bidx)) return n;
+        if (is_valid(n, aidx, bidx) || is_valid(n, bidx, aidx)) return n;
     }
     return -1;
 }
@@ -77,9 +94,12 @@ int main() {
             string sb = words[j];
             vector<int> aidx, bidx;
             if (!is_pair(sa, sb, aidx, bidx)) continue;
-
             int sq = cal(aidx, bidx);
-            if (sq > max_sq) max_sq = sq, max_a = sa, max_b = sb;
+            if (sq > max_sq) {
+                max_sq = sq;
+                max_a = sa;
+                max_b = sb;
+            }
         }
     }
 
