cdee2c991d09b3763f7651aa1e849c5804bc295b
cyp <cyp@vm>
2019-03-08 12:54:57 +0800

    solving 244

--- hiho_w244_p1.cpp
+++ hiho_w244_p1.cpp
@@ -6,24 +6,29 @@ using namespace std;
 vector<int> vals;
 vector<int> metrics;
 int n, m;
-int mm;
+unsigned long mm;
 vector<vector<int>> res;
 
-int set_bit(int num, int n) { return num |= 1UL << n; }
+unsigned long set_bit(unsigned long num, int n) { return num |= 1UL << n; }
 
-int clear_bit(int num, int n) { return num &= ~(1UL << n); }
+unsigned long clear_bit(unsigned long num, int n) { return num &= ~(1UL << n); }
 
 void Solve() {
-    res = vector<vector<int>>(n, vector<int>(mm, -1));
+    res = vector<vector<int>>(n, vector<int>(mm + 1, 0));
     for (int i = 0; i < n; i++) res[i][0] = 0;
-    for (int i = 0; i < mm; i++) {
-        metrics[0] 
+    res[0][metrics[0]] = vals[0];
+    for (int i = 0; i <= mm; i++) cout << res[0][i] << " ";
+    cout << endl;
+
+    for (int i = 1; i < n; i++) {
+        for (int j = 0; j <= mm; j++) {
+            res[i][j] = max(res[i - 1][j & (~metrics[i])] + vals[i], res[i - 1][j]);
+            cout << res[i][j] << " ";
+        }
+        cout << endl;
     }
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < mm; j++) {
-        }
-    }
+    cout << res[n - 1][mm] << endl;
 }
 
 int main() {
@@ -37,19 +42,21 @@ int main() {
 
         cin >> n >> m;
         for (int i = 0; i < m; i++) mm = set_bit(mm, i);
+        // cout << mm << endl;
 
         for (int i = 0; i < n; i++) {
             int v, s;
             cin >> v >> s;
-            int mets = 0;
+            unsigned long mets = 0;
             for (int j = 0; j < s; j++) {
                 int m;
                 cin >> m;
-                mets = set_bit(mets, m);
+                mets = set_bit(mets, m - 1);
             }
 
             vals.emplace_back(v);
             metrics.emplace_back(mets);
+            // cout << i << " " << v << " " << mets << endl;
         }
 
         Solve();
