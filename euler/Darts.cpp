fd18b174fd9d408344a33395bec01db968245b12
cyp <cyp@vm>
2019-03-18 18:37:52 +0800

    sovling darts

---
+++ euler/Darts.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int NN = 100;
+
+int main() {
+    vector<int> vals = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 25};
+    vector<int> dvals;
+    vector<int> w;
+    for (int i = 0; i <= vals.size(); i++) {
+        w.push_back(vals[i]);
+        w.push_back(vals[i] * 2);
+        if (vals[i] != 25) w.push_back(vals[i] * 3);
+    }
+
+    // f(j, i) = f(j - 1, i) + f(j, i - w[j])
+    vector<int> res(NN + 1, -1);
+    res[0] = 1;
+    res[1] = 1;
+    for (int j = 1; j < w.size(); j++) {
+        for (int i = w[j]; i <= NN; i++) {
+            res[i] += res[i] + res[i - w[j]];
+        }
+    }
+
+    return 0;
+}
