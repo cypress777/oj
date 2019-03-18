37d728b76aeccbb22299c680802de8c934568e61
cyp <cyp@wjj>
2019-03-18 21:55:36 +0800

    solved 109

--- euler/Darts.cpp
+++ euler/Darts.cpp
@@ -7,27 +7,31 @@ int NN = 100;
 
 int main() {
     vector<int> vals = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 25};
-    vector<int> ids;
-    vector<int> w;
-    int id = 0;
-    for (int i = 0; i <= vals.size(); i++) {
+    vector<int> ids{};
+    vector<int> w{};
+    for (int i = 0; i < vals.size(); i++) {
         w.push_back(vals[i]);
-        id++;
         w.push_back(vals[i] * 2);
-        ids.push_back(id);
-        id++;
-        if (vals[i] != 25) w.push_back(vals[i] * 3), id++;
+        ids.push_back(w.size() - 1);
+        if (vals[i] != 25) w.push_back(vals[i] * 3);
     }
+    w.push_back(0);
 
-    // f(j, i) = f(j - 1, i) + f(j, i - w[j])
-    vector<int> res(NN + 1, -1);
-    res[0] = 1;
-    res[1] = 1;
-    for (int j = 1; j < w.size(); j++) {
-        for (int i = w[j]; i <= NN; i++) {
-            res[i] += res[i] + res[i - w[j]];
+    vector<int> f(NN, 0);
+    for (int i = 0; i < w.size(); i++) {
+        for (int j = i; j < w.size(); j++) {
+            if (w[i] + w[j] < 100) f[w[i] + w[j]]++;
         }
     }
+
+    long long res = 0; 
+    for (int id : ids) {
+        for (int i = 0; i < NN; i++) {
+            if (i + w[id] < NN) res += f[i];
+        }
+    }
+
+    cout << res << endl;
 
     return 0;
 }
