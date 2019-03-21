4798c7e69890ba0c0c47f4bca356abce9e31c5ac
cyp <cyp@vm>
2019-03-21 18:49:21 +0800

    solving w246

--- hiho/hiho_w246_p1.cpp
+++ hiho/hiho_w246_p1.cpp
@@ -8,13 +8,29 @@ using namespace std;
 int len;
 
 struct query {
-    int l, r;
+    int id, l, r;
     query() {};
-    query(int l_, int r_) : l(l_), r(r_) {};
+    query(int id_, int l_, int r_) : id(id_), l(l_), r(r_) {};
 };
 
 bool comp(query a, query b) {
     return (a.l / len == a.l / len ? a.l / len < b.l / len : a.r / len < b.r / len);
+}
+
+int lowbit(int n) {
+    return n & ~(n - 1);
+}
+
+int get(const vector<int> &s, int n) {
+
+}
+
+int set(vector<int> &s, int n) {
+
+}
+
+void del(vector<int> &s, int n) {
+
 }
 
 void Solve() {
@@ -23,15 +39,21 @@ void Solve() {
     len = (int)sqrt(n);
     vector<int> a(n, -1);
     vector<query> q(m, query());
+    vector<int> s;
 
     for (int i = 0; i < n; i++) cin >> a[i];
     for (int i = 0; i < m; i++) {
         int l, r;
         cin >> l >> r;
-        q[i] = move(query(l, r));
+        q[i] = move(query(i, l, r));
     }
 
+    sort(q.begin(), q.end(), comp);
 
+    int pl = -1, pr = -1;
+    for (int i = 0; i < q.size(); i++) {
+
+    }
 }
 
 int main() {
