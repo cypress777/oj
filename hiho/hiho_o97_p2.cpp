f0a9912e76a95e02ba3b01b34597f6d77715f360
cyp <cyp@vm>
2019-03-24 14:49:11 +0800

    solving p2 p3

--- hiho/hiho_o97_p2.cpp
+++ hiho/hiho_o97_p2.cpp
@@ -1,4 +1,94 @@
-//
-// Created by hypereal on 3/24/19.
-//
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <iomanip>
 
+using namespace std;
+
+double err = 10e-8;
+
+struct Point {
+    double x, y;
+    Point() {};
+    Point(double x_, double y_) : x(x_), y(y_) {}
+};
+
+bool comp(const Point &a, const Point &b) {
+    return a.x > b.x;
+}
+
+bool on_line(const Point &a, const Point &b, const Point &c) {
+    return fabs((b.y - a.y) * (c.x - a.x) - (c.y - a.y) * (b.x - a.x)) < err;
+}
+
+vector<Point> points;
+
+int main() {
+    int N;
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        double x, y;
+        cin >> x >> y;
+        points.emplace_back(Point(x, y));
+    }
+
+    sort(points.begin(), points.end(), comp);
+
+    vector<Point> line_a(1, points[0]);
+    vector<Point> line_b{};
+
+    int find_a = false;
+    int i = 1, j = 1;
+    while (i < points.size()) {
+        if (fabs(points[i].x - points[i - 1].x) < err && fabs(points[i].y - points[i - 1].y) < err) {
+            i++;
+            continue;
+        }
+        for (j = i + 1; j < points.size(); j++) {
+            if (fabs(points[j].x - points[j - 1].x) < err && fabs(points[j].y - points[j - 1].y) < err) continue;
+            if (on_line(line_a[0], points[i], points[j])) {
+                if (!find_a) {
+                    find_a = true;
+                    line_a.emplace_back(points[i]);
+                    for (int k = i + 1; k < j; k++) {
+                        line_b.emplace_back(points[k]);
+                    }
+                }
+                line_a.emplace_back(points[j]);
+            } else {
+                if (find_a) line_b.emplace_back(points[j]);
+            }
+        }
+        if (!find_a) {
+            i++;
+        } else {
+            break;
+        }
+    }
+
+    double a1 = 0, a2 = 0, b1 = 0, b2 = 0;
+    for (int i = 1; i < line_a.size(); i++) {
+        a1 += (line_a[i].y - line_a[i - 1].y) / (line_a[i].x - line_a[i - 1].x);
+    }
+    a1 /= (line_a.size() - 1.0);
+    for (int i = 0; i < line_a.size(); i++) {
+        b1 += (line_a[i].y - line_a[i].x * a1);
+    }
+    b1 /= line_a.size();
+
+    for (int i = 1; i < line_b.size(); i++) {
+        a2 += (line_b[i].y - line_b[i - 1].y) / (line_b[i].x - line_b[i - 1].x);
+    }
+    a2 /= (line_b.size() - 1.0);
+    for (int i = 0; i < line_b.size(); i++) {
+        b2 += (line_b[i].y - line_b[i].x * a2);
+    }
+    b2 /= line_b.size();
+
+    if (a1 > a2) {
+        cout << fixed << setprecision(1) << a1 << " " << b1 << " " << a2 << " " << b2 << endl;
+    } else {
+        cout << fixed << setprecision(1) << a2 << " " << b2 << " " << a1 << " " << b1 << endl;
+    }
+}
--- hiho/hiho_o97_p3.cpp
+++ hiho/hiho_o97_p3.cpp
@@ -1,4 +1,54 @@
-//
-// Created by hypereal on 3/24/19.
-//
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <unordered_set>
 
+using namespace std;
+
+int N, M;
+vector<vector<int>> vex;
+vector<unordered_set<int>> res;
+vector<int> flg;
+
+int main() {
+    cin >> N >> M;
+
+    vex = vector<vector<int>>(N + 1, vector<int>{});
+    for (int i = 1; i <= N; i++) vex[i].push_back(i);
+    res = vector<unordered_set<int>>(N + 1, unordered_set<int>{});
+    flg = vector<int>(N + 1, -1);
+
+    for (int i = 0; i < M; i++) {
+        int u, v;
+        cin >> u >> v;
+        vex[u].push_back(v);
+    }
+
+    for (int i = 1; i <= N; i++) {
+        vector<int> vst(N + 1, -1);
+        queue<int> next{};
+
+        for (int j = 0; j < vex[i].size(); j++) next.push(vex[i][j]);
+
+        while(!next.empty()) {
+            int id = next.front();
+            next.pop();
+            if (vst[id] != -1) continue;
+            vst[id] = 1;
+            res[i].insert(id);
+            if (flg[id] != -1) {
+                for (auto v : res[id]) res[i].insert(v);
+                continue;
+            } else {
+                for (int j = 0; j < vex[id].size(); j++) next.push(vex[id][j]);
+            }
+        }
+        flg[i] = 1;
+    }
+
+    for (int i = 1; i <= N; i++) {
+        cout << res[i].size() << endl;
+    }
+
+    return 0;
+}
