6559279075cabe18fb04612e26f5c44ad50ebad5
cyp <cyp@wjj>
2019-03-26 00:42:37 +0800

    solved hiho o97 p4

--- hiho/hiho_o97_p4.cpp
+++ hiho/hiho_o97_p4.cpp
@@ -11,12 +11,12 @@ struct SegmentTree {
     SegmentTree() {};
 
     SegmentTree(int n, const vector<int> &arr) {
-        tree_ = vector<int>(2 * n - 1);
-        Build(0, 0, n - 1, arr);
+        tree_ = vector<int>(6 * n);
+        Build(1, 0, n - 1, arr);
     }
 
     int GetMax(int root, int start, int end, int qstart, int qend) {
-//        cout << "get max: " << root << " " << start << " " << end << " " << qstart << " " << qend << endl;
+//        cout << "get max: " << root << " " << start << " " << end << " " << qstart << " " << qend << " " << tree_[root] << endl;
         if (qstart > end || qend < start) return -1;
         if (start >= qstart && end <= qend) return tree_[root];
         int mid = (start + end) / 2;
@@ -24,7 +24,7 @@ struct SegmentTree {
     }
 
     void Build(int root, int start, int end, const vector<int> &arr) {
-        if (start = end) {
+        if (start == end) {
             tree_[root] = arr[start];
         } else {
             int mid = (start + end) / 2;
@@ -49,12 +49,12 @@ vector<SegmentTree> vertical_tree;
 int main() {
     cin >> N >> M >> Q;
 
-    map = vector<vector<int>>(N, vector<int>(M, -1));
-    up_sum = vector<vector<int>>(N, vector<int>(M, 0));
-    down_sum = vector<vector<int>>(N, vector<int>(M, 0));
-    right_sum = vector<vector<int>>(N, vector<int>(M, 0));
-    left_sum = vector<vector<int>>(N, vector<int>(M, 0));
-    left_sum_T = vector<vector<int>>(M, vector<int>(N, 0));
+    map = vector<vector<int>>(N, vector<int>(M));
+    up_sum = vector<vector<int>>(N, vector<int>(M));
+    down_sum = vector<vector<int>>(N, vector<int>(M));
+    right_sum = vector<vector<int>>(N, vector<int>(M));
+    left_sum = vector<vector<int>>(N, vector<int>(M));
+    left_sum_T = vector<vector<int>>(M, vector<int>(N));
 
     for (int i = 0; i < N; i++) {
         string s;
@@ -122,8 +122,8 @@ int main() {
         int c_min = max(c1 - left_sum[r1][c1], c2 - left_sum[r2][c2]);
         int r_max = min(down_sum[r1][c1] + r1, down_sum[r2][c2] + r2);
         int r_min = max(r1 - up_sum[r1][c1], r2 - up_sum[r2][c2]);
-        if (c_max >= c_min && horizontal_tree[max(r1, r2)].GetMax(0, 0, M - 1, c_min, c_max) >= abs(r2 - r1) - 1 ||
-            r_max >= r_min && vertical_tree[max(c1, c2)].GetMax(0, 0, N - 1, r_min, r_max) >= abs(c2 - c1) - 1) {
+        if (c_max >= c_min && horizontal_tree[max(r1, r2)].GetMax(1, 0, M - 1, c_min, c_max) >= abs(r2 - r1) - 1 ||
+            r_max >= r_min && vertical_tree[max(c1, c2)].GetMax(1, 0, N - 1, r_min, r_max) >= abs(c2 - c1) - 1) {
             cout << 2 << endl;
             continue;
         }
