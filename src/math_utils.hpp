60fdee76b0e028978d052d125652ba2320c9f78c
cyp <cyp@vm>
2019-04-02 10:47:38 +0800

    modified math utils

--- src/math_utils.hpp
+++ src/math_utils.hpp
@@ -2,4 +2,30 @@
 
 #include <vector>
 
-void gen_prime_tab(int n, std::vector<int> &prime_tab);
+template <typename T>
+void gen_prime_tab(T n, std::vector<T> &prime_tab) {
+    int count = 0;
+    auto is_prime = std::vector<bool>(n + 1, true);
+    prime_tab = std::vector<T>{};
+
+    for (T i = 2; i < n; i++) {
+        if (is_prime[i]) {
+            prime_tab.push_back(i);
+            count += 1;
+        }
+
+        for (T p : prime_tab) {
+            if (p * i > n) break;
+            is_prime[p * i] = false;
+            if (i % p == 0) break;
+        }
+    }
+}
+
+template <typename T>
+bool isPrime(T n, const std::vector<T> &prime_tab) {
+    for (T p : prime_tab) {
+        if (n % p == 0) return false;
+        if (p * p > n) return true;
+    }
+}
