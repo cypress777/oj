c87e4b7fcacf938a200d60c0e303e52eb06f1346
cyp <cyp@robot>
2019-04-12 15:05:30 +0800

    solving 1185

--- hiho/hiho1185.cpp
+++ hiho/hiho1185.cpp
@@ -7,8 +7,6 @@ int N, M;
 vector<int> W;
 vector<vector<int>> G;
 vector<vector<int>> G_r;
-vector<long long> W_n;
-vector<vector<int>> G_n;
 vector<int> flg;
 vector<int> post_order;
 
@@ -30,29 +28,52 @@ void reverse_graph() {
     }
 }
 
-long long dfs_by_order(int start) {
+long long dfs_by_order(int start, vector<int> &grp) {
     long long val = W[start];
+    grp.push_back(start);
 
     for (int id : G_r[start]) {
         if (flg[id] == -1) {
             flg[id] = 1;
             val += W[id];
-            dfs_by_order(id);
+            dfs_by_order(id, grp);
         }
     }
 
     return val;
 }
 
+bool not_cc(int p, int q) {
+    bool p2q = false, q2p = false;
+    for (int id : G[p]) {
+        if (id == q) {
+            p2q = true;
+            break;
+        }
+    }
+
+    for (int id : G[q]) {
+        if (id == p) {
+            q2p = true;
+            break;
+        }
+    }
+
+    if (p2q && q2p) return false;
+    else return true;
+}
+
 long long dfs_get_max(int start) {
-    long long val = W_n[start];
+    long long val = W[start];
 
     long long increase = 0;
-    for (int id : G_n[start]) {
+    for (int id : G[start]) {
         if (flg[id] == -1) {
             flg[id] = 1;
-            long long tmp = dfs_get_max(id);
-            if (tmp > increase) increase = tmp;
+            if (not_cc(id, start)) {
+                long long tmp = dfs_get_max(id);
+                if (tmp > increase) increase = tmp;
+            }
         }
     }
 
@@ -63,12 +84,13 @@ int main() {
     cin >> N >> M;
     W = vector<int>(N + 1, 0);
     G = vector<vector<int>>(N + 1, vector<int>());
+    G_r = vector<vector<int>>(N + 1, vector<int>());
 
-    for (int i = 0; i < N; i++) cin >> W[i];
+    for (int i = 1; i <= N; i++) cin >> W[i];
     for (int i = 0; i < M; i++) {
         int u, v;
         cin >> u >> v;
-        G[v].push_back(u);
+        G[u].push_back(v);
     }
 
     flg = vector<int>(N + 1, -1);
@@ -78,21 +100,39 @@ int main() {
             dfs_get_post(i);
         }
     }
+//    for (int p : post_order) cout << p << " ";
+//    cout << endl;
+//    cout << "--------" << endl;
 
     reverse_graph();
+//    for (int i = 1; i <= N; i++) {
+//        for (int p : G[i]) cout << p << " ";
+//        cout << endl;
+//    }
+//    cout << "--------" << endl;
+//
+//    for (int i = 1; i <= N; i++) {
+//        for (int p : G_r[i]) cout << p << " ";
+//        cout << endl;
+//    }
 
-    G_n = G;
-    W_n = vector<long long>(N + 1);
     flg = vector<int>(N + 1, -1);
-    for (int id : post_order) {
+    for (int i = 0; i < post_order.size(); i++) {
+        int id = post_order[post_order.size() - 1 - i];
         if (flg[id] == -1) {
             flg[id] = 1;
-            W_n[id] = dfs_by_order(id);
-            G_n[id] = vector<int>();
+            vector<int> cc;
+            long long cc_val = dfs_by_order(id, cc);
+            for (int cc_id : cc) {
+                cout << cc_id << " ";
+                W[cc_id] = cc_val;
+            }
+            cout << endl;
         }
     }
 
     flg = vector<int>(N + 1, -1);
+    flg[1] = 1;
     long long res = dfs_get_max(1);
 
     cout << res << endl;
