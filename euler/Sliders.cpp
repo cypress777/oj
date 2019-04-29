54f330424f45799c425e40bf3563a374289f1d97
cyp <cyp@wjj>
2019-04-29 23:24:52 +0800

    solve 244

--- euler/Sliders.cpp
+++ euler/Sliders.cpp
@@ -2,86 +2,66 @@
 #include <iostream>
 #include <queue>
 #include <unordered_map>
+#include <algorithm>
 
 using namespace std;
 
 vector<int> value;
-int MOD = 10000007;
+int MOD = 100000007;
+int LEN = 4;
 
 int encode(const vector<int> &map, int id) {
     int code = 0;
-    for (int i = 0; i < map.size(); i++) {
+    for (int i = 0; i < LEN * LEN; i++) {
         if (map[i] == 1) code |= (1UL << i);
     }
 
-    code |= (id << 16);
+    code |= (id << LEN * LEN);
     return code;
 }
 
-void decode(int code, vector<int> & map, int &id) {
-    map = vector<int>(16);
+void decode(int code, vector<int> &map, int &id) {
+    map = vector<int>(LEN * LEN, 0);
     for (int i = 0; i < 16; i++) {
         if (code & (1UL << i)) map[i] = 1;
     }
-    id = (code >> 16);
-}
-
-int get_next(int code , int dir) {
-    vector<int> map;
-    int id;
-    decode(code, map, id);
-
-    vector<int> nmap = map;
-    int nid = -1;
-    if (dir == 0) {
-        if (nid / 4 == 0) nid = -1;
-        else nid = id - 4;
-    } else if (dir == 1) {
-        if (nid % 4 == 3) nid = -1;
-        else nid = id + 1;
-    } else if (dir == 2) {
-        if (nid / 4 == 3) nid = -1;
-        else nid = id + 4;
-    } else if (dir == 3) {
-        if (nid %4 == 0) nid = -1;
-        else nid = id - 1;
-    }
-
-    if (nid >= 0) {
-        nmap[id] = map[nid];
-        nmap[nid] = 0;
-        return encode(nmap, nid);
-    }
-    return -1;
+    id = (code >> LEN * LEN);
 }
 
 void show(int code) {
     if (code == -1) {
-        cout << "map: none id: " << -1 << endl;
+        cout << "code: " << -1 << endl;
         return;
     }
+
     vector<int> map;
     int id;
     decode(code, map, id);
-    cout << "map: ";
-    for (int i = 0; i < 16; i++) cout << map[i] << " ";
-    cout << "id: " << id << endl;
+    cout << "code: " << code << " map: " << endl;
+    for (int i = 0; i < LEN; i++) {
+        for (int j = 0; j < LEN; j++) {
+            if (i * LEN + j == id) cout << "+ ";
+            else cout << map[i * LEN + j] << " ";
+        }
+        cout << endl;
+    }
+
 }
 
 int main() {
-    vector<int> start_map(16);
-    for (int i = 0; i <  4; i++) {
-        for (int j = 0; j < 4; j++) {
-            if (j > 1) start_map[i * 4 + j] = 1;
+    vector<int> start_map(LEN * LEN, 0);
+    for (int i = 0; i < LEN; i++) {
+        for (int j = 0; j < LEN; j++) {
+            if (j > 1) start_map[i * LEN + j] = 1;
         }
     }
     int start_code = encode(start_map, 0);
     show(start_code);
 
-    vector<int> end_map(16);
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
-            if ((i % 2 == 0 && j % 2 == 1) || (i % 2 == 1 && j % 2 == 0)) end_map[i * 4 + j] = 1;
+    vector<int> end_map(LEN * LEN, 0);
+    for (int i = 0; i < LEN; i++) {
+        for (int j = 0; j < LEN; j++) {
+            if ((i % 2 == 0 && j % 2 == 1) || (i % 2 == 1 && j % 2 == 0)) end_map[i * LEN + j] = 1;
         }
     }
     int end_code = encode(end_map, 0);
@@ -94,34 +74,52 @@ int main() {
 
     value = {68, 76, 85, 82};
 
+    int n = 1;
+    vector<int> states;
+    states.push_back(start_code);
+
+    vector<int> dx = {-1, 0, 1, 0};
+    vector<int> dy = {0, 1, 0, -1};
+
     while (!que.empty()) {
         int code = que.front();
         que.pop();
-        cout << "que size: " << que.size() << " code: " << code << " code val: " << vst[code] << " ";
+
+        cout << "-------" << endl;
         show(code);
+
+        vector<int> map;
+        int id;
+        decode(code, map, id);
+        int x = id / LEN;
+        int y = id % LEN;
+
         cout << "next: " << endl;
+
         for (int i = 0; i < 4; i++) {
-            cout << "111" << endl;
-            int next_code = get_next(code, i);
-            cout << "2222" << endl;
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (nx < 0 || nx >= LEN || ny < 0 || ny >= LEN) continue;
+
+            int nid = nx * LEN + ny;
+            vector<int> nmap = map;
+            nmap[nid] = 0;
+            nmap[id] = map[nid];
+            int next_code = encode(nmap, nid);
+            show(next_code);
+
             if (next_code == end_code) {
                 cout << "result: " << ((vst[code] * 243 + value[i]) % MOD) << endl;
                 return 0;
             }
-            cout << "333" << endl;
-            show(next_code);
-            vst.find(next_code);
-            cout << "+++" << endl;
+
             if (next_code >= 0 && vst.find(next_code) == vst.end()) {
-                cout << "777" << endl;
                 vst[next_code] = (vst[code] * 243 + value[i]) % MOD;
-                cout << "666" << endl;
                 que.push(next_code);
-                cout << "555" << endl;
+                n++;
+                states.push_back(next_code);
             }
-            cout << "4444" << endl;
         }
-        cout << "222" << endl;
     }
 
     return 0;
