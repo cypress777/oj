f79301a5962b82598f94122c79e303490b0bdfc0
cyp <cyp@vm>
2019-05-13 18:36:52 +0800

    solving 250

--- hiho/hiho_w250_p1.cpp
+++ hiho/hiho_w250_p1.cpp
@@ -8,37 +8,36 @@ vector<int> r_sockets;
 vector<int> l_sockets;
 int MM = 1000000007;
 vector<vector<int>> cal_map;
+
 void r2l() {
     int val = 0;
-    int last_r_cnt = 0;
     int r_cnt = 0;
 
     for (int i = ori.size() - 1; i >= 0; i++) {
         val += ori[i];
-        if (ori[i] == 1) r_cnt++;
-        if (val > 0) {
-            val = 0;
-            r_sockets.push_back(r_cnt - last_r_cnt);
-            last_r_cnt = r_cnt;
+        if (ori[i] == 1) {
+            if (val > 0) {
+                r_cnt++;
+                val = 0;
+            }
+            r_sockets.push_back(r_cnt);
         }
-        i--;
     }
 }
 
 void l2r() {
     int val = 0;
-    int last_l_cnt = 0;
     int l_cnt = 0;
 
     for (int i = 0; i < ori.size(); i++) {
         val += ori[i];
-        if (ori[i] == -1)  l_cnt++;
-        if (val < 0) {
-            val = 0;
-            l_sockets.push_back(l_cnt - last_l_cnt);
-            last_l_cnt = l_cnt;
+        if (ori[i] == -1) {
+            if (val < 0) {
+                l_cnt++;
+                val = 0;
+            }
+            l_sockets.push_back(l_cnt);
         }
-        i++;
     }
 }
 
@@ -59,15 +58,15 @@ int solve(vector<int> socket) {
 
     vector<vector<int>> res(cnt, vector<int>(cnt + 1, 0));
 
-    res[0][1] = cal_map[socket[cnt - 1]][1];
-    res[0][0] = 1;
+    for (int i = 1; i < cnt; i++) res[0][i] = cal_map[socket[0]][i];
 
+    //  res[i][j] put j para before i
     for (int i = 1; i < cnt; i++) {
-        int limit = i + 1;
-        for (int j = 0; j <= limit; j++) {
-            for (int k = 0; k <= j ; k++) {
-                if (j - k > limit - 1) continue;
-                res[i][j] += res[i - 1][j - k] * cal_map[socket[cnt - 1 - i]][k] % MM;
+        int min_limit = i + 1;
+        for (int j = min_limit; j <= cnt; j++) {l_sockets
+            int last_min_limit = i;
+            for (int k = last_min_limit; k <= j; k++) {
+                res[i][j] += res[i - 1][k] * cal_map[socket[i]][j - k] % MM;
                 res[i][j] %= MM;
             }
         }
