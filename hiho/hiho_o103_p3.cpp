218b5624026d11787a4638c9371ed16f9891b87b
cyp <cyp@wjj>
2019-06-17 00:04:35 +0800

    solved 103 p3

--- hiho/hiho_o103_p3.cpp
+++ hiho/hiho_o103_p3.cpp
@@ -1,51 +1,113 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <unordered_map>
 #include <unordered_set>
 
 using namespace std;
 
-int n, m, k;
-unordered_set<int> row;
-unordered_set<int> col;
-
-vector<int> r_start;
-vector<int> r_end;
-vector<int> c_start;
-vector<int> c_end;
-vector<int> res;
+long long n, m, k;
+unordered_set<int> r_set;
+unordered_set<int> c_set;
+vector<int> r;
+vector<int> c;
+unordered_map<int, vector<int>> rows;
+unordered_map<int, vector<int>> cols;
 
 int main() {
+    long long res1 = 0, res11 = 0, res2 = 0, res21 = 0, res22 = 0, res0 = 0, light_cnt = 0;
+
     cin >> n >> m >> k;
 
-    r_start = vector<int>(n, m);
-    r_end = vector<int>(n, -1);
-    c_start = vector<int>(m, n);
-    c_end = vector<int>(m, -1);
-
     for (int i = 0; i < k; i++) {
-        int r, c;
-        cin >> r > c;
-        row.insert(r);
-        col.insert(c);
+        int rr, cc;
+        cin >> rr >> cc;
 
-        r_start[r] = min(c, r_start[r]);
-        r_end[r] = max(c, r_end[r]);
-        c_start[c] = min(r, c_start[c]);
-        c_end[c] = max(r, c_end[c]);
+        light_cnt++;
+
+        r_set.insert(rr);
+        c_set.insert(cc);
+
+        if (rows.find(rr) == rows.end()) {
+            rows[rr] = {cc};
+        } else {
+            int has = false;
+            for (int k : rows[rr]) if (k == cc) has = true;
+            if (!has) rows[rr].push_back(cc);
+        }
+        if (cols.find(cc) == cols.end()) {
+            cols[cc] = {rr};
+        } else {
+            int has = false;
+            for (int k : cols[cc]) if (k == rr) has = true;
+            if (!has) cols[cc].push_back(rr);
+        }
     }
 
-    res = vector<int>(5, 0);
+    for (int i : r_set) r.push_back(i);
+    for (int i : c_set) c.push_back(i);
 
-    for (int i : row) {
-        if (r_start[i] < r_end[i]) {
-            for (int j : col) {
-                if (j < r_start[i] || j > r_end[i]) continue;
-                if (c_start[j] < i && c_end[j] > i) {
-                    res[4]++;
+    sort(r.begin(), r.end());
+    sort(c.begin(), c.end());
+
+    for (int i : r) {
+        sort(rows[i].begin(), rows[i].end());
+    }
+    for (int i : c) {
+        sort(cols[i].begin(), cols[i].end());
+    }
+
+    for (int i : r) {
+        res1 += rows[i][0];
+        res1 += n - 1 - rows[i][rows[i].size() - 1];
+
+        int start = rows[i][0];
+        for (int end : rows[i]) {
+            if (end == start) continue;
+            res2 += end - start - 1;
+            start = end;
+        }
+    }
+
+    for (int i : c) {
+        res1 += cols[i][0];
+        res1 += m - 1 - cols[i][cols[i].size() - 1];
+
+        int start = cols[i][0];
+        for (int end : cols[i]) {
+            if (end == start) continue;
+            res2 += end - start - 1;
+            start = end;
+        }
+    }
+
+    for (int i : r) {
+        for (int j : c) {
+            int light = false;
+            for (int k : cols[j]) {
+                if (k == i) {
+                    light = true;
+                    break;
                 }
+            };
+            if (light) continue;
+
+            if ((j < rows[i][0] || j > rows[i][rows[i].size() - 1]) && (i < cols[j][0] || i > cols[j][cols[j].size() - 1])) {
+                res11++;
+            } else if ((j < rows[i][0] || j > rows[i][rows[i].size() - 1]) || (i < cols[j][0] || i > cols[j][cols[j].size() - 1])) {
+                res21++;
+            } else {
+                res22++;
             }
         }
     }
+
+    res1 -= (res21 + 2 * res11);
+    res2 -= (res21 + 2 * res22);
+
+    res0 = n * m - res1 - res2 - res11 - res21 - res22 - light_cnt;
+
+    cout << res0 << " " << res1 << " " << res11 + res2 << " " << res21 << " " << res22 << endl;
 
     return 0;
 }
