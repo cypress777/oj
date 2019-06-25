e1639e1204584756834686b8ba742c7ef0e07bdb
cyp <cyp@vm>
2019-06-25 13:04:49 +0800

    solving ch1 p1

--- hiho/hiho_ch1_p1.cpp
+++ hiho/hiho_ch1_p1.cpp
@@ -8,13 +8,13 @@ long long MM = 1000000007;
 vector<vector<long long>> sum_lut;
 vector<vector<long long>> cnt_lut;
 
-long long mod_pow(long long base, long long power) {
-    long long res = 1, num = base;
-    while (power > 0) {
-        if (power % 2 == 1) res = (res * num) % MM;
-        num = (num * base) % MM;
-        power /= 2;
-    }
+void cal(int k, long long l, long long r, long long &res, long long &cnt);
+void cal_same(int len, int k, long long limit_min, long long limit_max, long long &res, long long &cnt);
+
+int get_len(long long n) {
+    int len = 0;
+    while(n > 0) len++, n /= 10;
+    return len;
 }
 
 long long mult_pow10(long long n, int t) {
@@ -22,7 +22,24 @@ long long mult_pow10(long long n, int t) {
     return n;
 }
 
-void cal(int len, int k, long long limit_min, long long limit_max, long long &res, long long &cnt) {
+void cal(int k, long long l, long long r, long long &res, long long &cnt) {
+    int len_min = get_len(l), len_max = get_len(r);
+
+    if (len_min == len_max) {
+        cal_same(len_min, k, l, r, res, cnt);
+    } else {
+        cal_same(len_min, k, l, mult_pow10(1, len_min) - 1, res, cnt);
+        cal_same(len_max, k, mult_pow10(1, len_max - 1), r, res, cnt);
+
+        for (int len = len_min + 1; len < len_max; len++) {
+            cal_same(len, k, mult_pow10(1, len - 1), mult_pow10(10L, len) - 1, res, cnt);
+        }
+    }
+}
+
+void cal_same(int len, int k, long long limit_min, long long limit_max, long long &res, long long &cnt) {
+    cout << len << " " << k << " " << limit_min << " " << limit_max << endl;
+    if (len < 1) return;
     if (len == 1) {
         if (k >= limit_min && k <= limit_max) {
             res++;
@@ -31,22 +48,27 @@ void cal(int len, int k, long long limit_min, long long limit_max, long long &re
         return;
     }
 
-    for (int i = limit_min / pow(10, len - 1) + 1; i <= limit_max / pow(10, len - 1); i++) {
+    for (int i = limit_min / mult_pow10(1, len - 1) + 1; i <= limit_max / mult_pow10(1, len - 1); i++) {
         if (i - k < -100 || i - k > 100) break;
 
-        long long tail = sum_lut[len - 1][i - k]) % MM;
+        long long tail = sum_lut[len - 1][i - k + 100] % MM;
 
-        long long top = cnt_lut[len - 1][i - k] * i % MM;
+        long long top = cnt_lut[len - 1][i - k + 100] * i % MM;
         mult_pow10(top, len - 1);
 
         res = (res + tail + top) % MM;
     }
 
-    long long lmin_top = limit_min / pow(10L, len - 1), lmax_top = limit_max / pow(10L, len - 1);
-    long long lmin_rear = limit_min % pow(10L, len - 1), lmax_rear = limit_max % pow(10L, len - 1);
+    long long lmin_top = limit_min / mult_pow10(1, len - 1), lmax_top = limit_max / mult_pow10(1, len - 1);
+
+    long long lmin_rear = limit_min % mult_pow10(1, len - 1), lmax_rear = limit_max % mult_pow10(1, len - 1);
+
+    int lmin_rear_len = get_len(lmin_rear), lmax_rear_len = get_len(lmax_rear);
+
     if (lmin_top == lmax_top) {
         long long tail_sum = 0, tail_cnt = 0;
-        cal(len - 1, lmin_top - k, lmin_rear, lmax_rear, tail_sum, tail_cnt);
+
+        cal(k - lmin_top, lmin_rear, lmax_rear, tail_sum, tail_cnt);
 
         long long top_sum = tail_cnt * lmin_top % MM;
         mult_pow10(top_sum, len - 1);
@@ -54,7 +76,8 @@ void cal(int len, int k, long long limit_min, long long limit_max, long long &re
         res = (res + tail_sum + top_sum) % MM;
     } else {
         long long lmin_tail_sum = 0, lmin_tail_cnt = 0;
-        cal(len - 1, lmin_top - k, lmin_rear, pow(10L, len - 1) - 1, lmin_tail_sum, lmin_tail_cnt);
+
+        cal(k - lmin_top, lmin_rear, mult_pow10(1, len - 1) - 1, lmin_tail_sum, lmin_tail_cnt);
 
         long long lmin_top_sum = lmin_tail_cnt * lmin_top % MM;
         mult_pow10(lmin_top_sum, len - 1);
@@ -62,7 +85,7 @@ void cal(int len, int k, long long limit_min, long long limit_max, long long &re
         res = (res + lmin_tail_sum + lmin_top_sum) % MM;
 
         long long lmax_tail_sum = 0, lmax_tail_cnt = 0;
-        cal(len - 1, lmin_top - k, lmin_rear, pow(10L, len - 1) - 1, lmax_tail_sum, lmax_tail_cnt);
+        cal(k - lmax_top, lmax_rear, mult_pow10(1, len - 1) - 1, lmax_tail_sum, lmax_tail_cnt);
 
         long long lmax_top_sum = lmax_tail_cnt * lmax_top % MM;
         mult_pow10(lmax_top_sum, len - 1);
@@ -75,47 +98,31 @@ int main() {
     long long l, r, k;
     cin >> l >> r >> k;
 
-    sum_lut = vector<vector<long long>>(10, vector<long long>(201, 0));
-    cnt_lut = vector<vector<long long>>(10, vector<long long>(201, 0));
-    cnt_lut[0][0] = 1;
+    sum_lut = vector<vector<long long>>(20, vector<long long>(201, 0));
+    cnt_lut = vector<vector<long long>>(20, vector<long long>(201, 0));
+    cnt_lut[0][100] = 1;
 
     // len
-    for (int i = 1; i <= 9; i++) {
+    for (int i = 1; i <= 18; i++) {
         // sum
-        int sign = (j % 2 == 1 ? 1 : -1);
-
+        int sign = (i % 2 == 1 ? 1 : -1);
         for (int j = -100; j <= 100; j++) {
-            for (n = 0; n <= 9; n++) {
-                if (j - sign * n > 100 || j - sign * n < -100) break;
-                sum_lut[i][j] = (sum_lut[i][j] + sum_lut[i - 1][j - sign * n] * 10 % MM + n * cnt_lut[i - 1][j - sign * n]) % MM;
-                cnt_lut[i][j] = (cnt_lut[i][j] + cnt_lut[i - 1][j - sign * n]) % MM;
+            for (int n = 0; n <= 9; n++) {
+                if (j - sign * n > 100 || j - sign * n < -100) continue;
+
+                sum_lut[i][j + 100] = (sum_lut[i][j + 100] + sum_lut[i - 1][j - sign * n + 100] * 10 % MM + n * cnt_lut[i - 1][j - sign * n + 100]) % MM;
+                cnt_lut[i][j + 100] = (cnt_lut[i][j + 100] + cnt_lut[i - 1][j - sign * n + 100]) % MM;
             }
         }
     }
 
-    int len_min = 0, len_max = 0;
-
-    int ll = l;
-    while (ll > 0) len_min++, ll /= 10;
-
-    int rr = r;
-    while (rr > 0) len_max++, rr /= 10;
+    int len_min = get_len(l), len_max = get_len(r);
 
     long long res = 0;
     long long cnt = 0;
-    if (len_min == len_max) {
-        cal(len_min, k, l, r, res, cnt);
-    } else {
-        cal(len_min, k, l, pow(10L, len_min) - 1, res, cnt);
-        cal(len_max, k, pow(10L, len_max - 1), r, res, cnt);
-
-        for (int len = len_min + 1; len < len_max; len++) {
-            res + cal(len, k, pow(10L, len - 1), pow(10L, len) - 1, res, cnt);
-        }
-    }
+    cal(k, l, r, res, cnt);
 
     cout << res << endl;
 
     return 0;
 }
-
