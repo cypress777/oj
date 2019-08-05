53cf05a623fbf4156dcb572236f0a433f5980d09
cyp <cyp@vm>
2019-08-05 15:03:40 +0800

    solving 106

---
+++ hiho/hiho_o106_p2.cpp
@@ -0,0 +1,42 @@
+#include <vector>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string number;
+    cin >> number;
+
+    vector<int> max_val(number.size());
+    vector<int> max_id(number.size());
+
+    int cur_max = 0;
+    int cur_max_id = -1;
+    for (int i = number.size() - 1; i >= 0; i--) {
+        int v = number[i] - '0';
+
+        if (v > cur_max) {
+            cur_max = v;
+            cur_max_id = i;
+        }
+
+        max_val[i] = cur_max;
+        max_id[i] = cur_max_id;
+    }
+
+    for (int i = 0; i < number.size() - 1; i++) {
+        int n = number[i] - '0';
+        if (max_val[i + 1] > n) {
+            int tmp = max_val[i + 1];
+            number[max_id[i + 1]] = n + '0';
+            number[i] = tmp + '0';
+            break;
+        }
+    }
+
+    cout << number << endl;
+
+    return 0;
+}
+
