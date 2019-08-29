0c27c38e0c22dbe5af767887e7c59d71f4585cca
cyp <cyp@vm>
2019-08-29 12:30:39 +0800

    solving 106

--- hiho/hiho_o106_p4.cpp
+++ hiho/hiho_o106_p4.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
-int N, K, x_rem;
-vector<int> ptab, fcts;
+long long N, K, x_rem;
+vector<long long> ptab, ftab;
 
-template <typename T>
+template<typename T>
 void gen_prime_tab(T n, std::vector<T> &prime_tab) {
-    int count = 0;
+    long long count = 0;
     auto is_prime = std::vector<bool>(n + 1, true);
     prime_tab = std::vector<T>{};
 
-    for (T i = 2; i < n; i++) {
+    for (T i = 2; i <= n; i++) {
         if (is_prime[i]) {
             prime_tab.push_back(i);
             count += 1;
@@ -26,30 +27,44 @@ void gen_prime_tab(T n, std::vector<T> &prime_tab) {
     }
 }
 
-void get_factors(vector<int> &fcts, const vector<int> &ptab) {
-    int nn = n;
+void get_factors(vector<long long> &ftab, const vector<long long> &ptab) {
+    long long kk = K;
     for (auto p : ptab) {
-        if (nn % p == 0) fcts.push_back(p);
+        if (kk % p == 0) ftab.push_back(p);
 
-        while (nn % p == 0) nn /= p;
+        while (kk % p == 0) kk /= p;
 
-        if (nn == 0) break;
+        if (kk == 0) break;
     }
 }
 
-void calc(int cnt, int last_id, int cur_cnt, int cur_fct, int &res) {
+void calc(long long cnt, long long last_id, long long cur_cnt, long long cur_fct, long long &res) {
     if (cnt == cur_cnt) {
-        int nf = (K + 1) / cur_fct;
+        long long n_f = (K + 1) / cur_fct;
 
-        int t = 1;
+        long long t = 1;
         while ((cur_fct * t) % 3 != x_rem) t++;
 
-        res += ((K + 1) / 3 + ((K + 1) % 3 >= (x_rem + 1) ? 1 : 0)) - nf / 3 + (nf % 3 >= t ? 1 : 0);
+        long long n_rem = (K + 1) / 3 + ((K + 1) % 3 >= (x_rem + 1) ? 1 : 0);
+        long long n_f_x = n_f / 3 + (n_f % 3 >= t ? 1 : 0);
+        res += n_rem - n_f_x;
     } else {
-        for (int i = last_id + 1; i < fcts.size(); i++) {
-            calc(cnt, i, cur_cnt + 1, cur_fct * fcts[i], res);
+        for (long long i = last_id + 1; i < ftab.size(); i++) {
+            calc(cnt, i, cur_cnt + 1, cur_fct * ftab[i], res);
         }
     }
+}
+
+long long C(long long a, long long b) {
+    long long res = 1;
+
+    b = max(a - b, b);
+
+    for (long long i = a; i > b; i--) res = res * i;
+
+    for (long long i = a - b; i > 0; i--) res /= i;
+
+    return res;
 }
 
 int main() {
@@ -58,23 +73,33 @@ int main() {
     K = (N + 3) / 2;
     x_rem = 3 - K % 3;
 
-    int res = 0;
+    long long res = 0;
 
-    if (x_rem == 3) {
-        cout << res << endl;
+    if (N == 1) {
+        res = 1;
+    } else if (x_rem == 3) {
+        res = 0;
     } else {
-        gen_prime_tab(n, ptab);
+        gen_prime_tab(static_cast<long long>(sqrt(K + 1)), ptab);
 
-        get_factors(fcts, ptab);
+        get_factors(ftab, ptab);
+        if (ftab.size() == 0) ftab.push_back(K);
 
-        int sign = 1;
-        for (int i = 0; i < fcts.size(); i++) {
-            int cur_res = 0;
+        for (long long i = 1; i <= ftab.size(); i++) {
+            long long cur_res = 0;
             calc(i, -1, 0, 1, cur_res);
 
-            res += sign * cur_res;
+            long long sign = 1;
+            long long cnt = 0;
+            for (long long j = 1; j < i; j++) {
+                cnt += sign * C(i, j);
+                sign *= -1;
+            }
+            res += (1 - cnt) * cur_res;
         }
     }
+
+    cout << res << endl;
 
     return 0;
 }
