1c286d69aa841afa49c9978be4f246e768af6f59
cyp <cyp@vm>
2019-09-10 16:53:25 +0800

    solving 270

--- hiho/hiho_w270_p1.cpp
+++ hiho/hiho_w270_p1.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
-#include <pair>
 
 using namespace std;
 
@@ -22,6 +21,7 @@ struct QuickUnion {
 
         root = vector<int>(len);
         size = vector<int>(len, 1);
+        areas = vector<Area>(len);
 
         for (int i = 0; i < len; i++) {
             root[i] = i;
@@ -35,14 +35,14 @@ struct QuickUnion {
         }
     }
 
-    void get_root(int i) {
+    int get_root(int i) {
         int r = root[i];
         if (r == i) return i;
         root[i] = get_root(r);
         return root[i];
     }
 
-    void get_area(int i) {
+    Area get_area(int i) {
         int r = get_root(i);
         return areas[r];
     }
@@ -68,41 +68,26 @@ struct QuickUnion {
         int left_i = areas[root_i].left - 1;
         if (left_i >= 0) quick_union(left_i, i);
 
-        int root_ight_i = areas[ri].right + 1;
+        int right_i = areas[root_i].right + 1;
         if (right_i < areas.size()) quick_union(right_i, i);
     }
 };
 
-void fill(const pair<int, double> &injection, vector<pair<int, double>> &new_injections, vector<double> &rt_h) {
-    double bottom = area.right - area.left + 1;
-    double max_water_height = area.height / bottom;
-    double max_volume_height = min(height[area.left - 1] - height[area.left],
-                                   height[area.right + 1] - height[area.right]);
-    double increase_height = min(max_water_height, max_volume_height);
-
-    for (int i = area.left; i <= area.right; i++) {
-        height[i] += increase_height;
-    }
-
-    if (max_water_height > max_volume_height) {
-        double remain_height = (max_water_height - max_volume_height) * bottom;
-        new_areas.push_back(Area(area.left, area.right, remain_height));
-    }
-}
-
-pair<int, double> change(const vector<pair<int, double>> &injections, QuickUnion &rt_h) {
-    vector<Area> new_injections;
+vector<pair<int, double>> change(const vector<pair<int, double>> &injections, QuickUnion &rt_h) {
+    vector<pair<int, double>> new_injections;
 
 //    cout << "-----" << endl;
 
     for (auto injection : injections) {
+        int N = rt_h.areas.size();
+
+        if (injection.first < 0 || injection.first >= N) continue;
+
         Area area = rt_h.get_area(injection.first);
         Area left_area = (area.left > 0 ? rt_h.get_area(area.left - 1) : Area(-1, -1, -1000));
-        Area right_area = (area.right < rt_h.size() - 1 ?
-                           rt_h.get_area(area.right + 1) : Area(rt_h.size(), rt_h.size(), -1000));
+        Area right_area = (area.right < N - 1 ?
+                           rt_h.get_area(area.right + 1) : Area(N, N, -1000));
 //        cout << injection.first << " " << area.left << " " << area.right << " " << injection.second << endl;
-
-        if (area.left < 0 || area.right >= height.size()) continue;
 
         if (area.height > left_area.height && area.height > right_area.height) {
 //            cout << "split" << endl;
@@ -140,7 +125,7 @@ int main() {
     cin >> N >> S >> M >> T;
     S--, T--;
 
-    vector<double> ori_h(N);
+    vector<int> ori_h(N);
     for (int i = 0; i < N; i++) {
         cin >> ori_h[i];
     }
@@ -155,11 +140,12 @@ int main() {
         injections = change(injections, rt_h);
 
         cnt++;
-        if (cnt > 100) break;
+        if (cnt > 4) break;
     }
 
-//    cout << "==== res: " << endl;
-//    for (auto h : cur_h) cout << h << " ";
+//    for (int i = 0; i < ori_h.size(); i++) {
+//        cout << rt_h.get_area(i).height << " ";
+//    }
 //    cout << endl;
 
     cout << floor(rt_h.get_area(T).height - ori_h[T]) << endl;
