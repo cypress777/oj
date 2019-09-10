0aeb84bd3a20f7de3555c36110b965faddcf4b73
cyp <cyp@vm>
2019-09-10 12:55:38 +0800

    solving 270

--- hiho/hiho_w270_p1.cpp
+++ hiho/hiho_w270_p1.cpp
@@ -12,7 +12,7 @@ struct Area {
     Area(int l, int r, double w) : left(l), right(r), water(w) {}
 };
 
-void expand(const Area &area, vector<Area> &new_areas, vector<int> &height) {
+void expand(const Area &area, vector<Area> &new_areas, vector<double> &height) {
     auto new_area = area;
 
     while (new_area.left == 0 ||
@@ -28,26 +28,20 @@ void expand(const Area &area, vector<Area> &new_areas, vector<int> &height) {
     new_areas.push_back(new_area);
 }
 
-void transfer(const Area &area, vector<Area> &new_areas, vector<int> &height) {
-    auto new_area = area;
-
-    while (new_area.left > 0 && height[new_area.left] == height[new_area.left - 1]) {
-        new_area.left--;
+void transfer(const Area &area, vector<Area> &new_areas, vector<double> &height) {
+    if (area.left == 0 || height[area.left] > height[area.left - 1]) {
+        new_areas.push_back(Area(area.left - 1, area.left - 1, area.water));
+    } else {
+        new_areas.push_back(Area(area.right + 1, area.right + 1, area.water));
     }
-
-    while (new_area.right < height.size() - 1 && height[new_area.right] == height[new_area.right + 1]) {
-        new_area.right++;
-    }
-
-    new_areas.push_back(new_area);
 }
 
-void split(const Area &area, vector<Area> &new_areas, vector<int> &height) {
+void split(const Area &area, vector<Area> &new_areas, vector<double> &height) {
     new_areas.push_back(Area(area.left - 1, area.left - 1, area.water / 2.0));
     new_areas.push_back(Area(area.right + 1, area.right + 1, area.water / 2.0));
 }
 
-void fill(const Area &area, vector<Area> &new_areas, vector<int> &height) {
+void fill(const Area &area, vector<Area> &new_areas, vector<double> &height) {
     double bottom = area.right - area.left + 1;
     double max_water_height = area.water / bottom;
     double max_volume_height = min(height[area.left - 1] - height[area.left],
@@ -64,22 +58,30 @@ void fill(const Area &area, vector<Area> &new_areas, vector<int> &height) {
     }
 }
 
-vector<Area> change(const vector<Area> &areas, vector<int> &height) {
+vector<Area> change(const vector<Area> &areas, vector<double> &height) {
     vector<Area> new_areas;
 
+    cout << "-----" << endl;
+
     for (auto area : areas) {
+        cout << area.left << " " << area.right << " " << area.water << endl;
+
         if (area.left < 0 || area.right >= height.size()) continue;
 
         if ((area.left > 0 && height[area.left] == height[area.left - 1]) ||
             (area.right < height.size() - 1 && height[area.right] == height[area.right + 1])) {
+            cout << "expand" << endl;
             expand(area, new_areas, height);
         } else if ((area.left == 0 || height[area.left] > height[area.left - 1]) &&
                    (area.right == height.size() - 1 || height[area.right] > height[area.right + 1])) {
+            cout << "split" << endl;
             split(area, new_areas, height);
         } else if ((area.left == 0 || height[area.left] > height[area.left - 1]) ||
                    (area.right == height.size() - 1 || height[area.right] > height[area.right + 1])) {
+            cout << "transfer" << endl;
             transfer(area, new_areas, height);
         } else {
+            cout << "fill" << endl;
             fill(area, new_areas, height);
         }
     }
@@ -92,19 +94,29 @@ int main() {
     cin >> N >> S >> M >> T;
     S--, T--;
 
-    vector<int> ori_h(N);
+    vector<double> ori_h(N);
     for (int i = 0; i < N; i++) {
         cin >> ori_h[i];
     }
 
-    vector<int> cur_h = ori_h;
+    vector<double> cur_h = ori_h;
 
     vector<Area> areas(1);
     areas[0] = Area(S, S, double(M));
 
-    while (areas.empty()) {
-        auto areas = change(areas, cur_h);
+    int cnt = 0;
+    while (!areas.empty()) {
+        areas = change(areas, cur_h);
+
+        cnt++;
+        if (cnt > 100) break;
     }
+
+    cout << "==== res: " << endl;
+    for (auto h : cur_h) cout << h << " ";
+    cout << endl;
+
+    cout << floor(cur_h[T] - ori_h[T]) << endl;
 
     return 0;
 }
