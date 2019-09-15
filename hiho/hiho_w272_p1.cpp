71cbf38b94697f431c2d293f1950260ca58ff208
cypress777 <chenypress@gmail.com>
2019-09-15 17:55:14 +0800

    solving w272

--- hiho/hiho_w272_p1.cpp
+++ hiho/hiho_w272_p1.cpp
@@ -34,14 +34,36 @@ int main() {
     }
 
     vector<int> que;
+    vector<int> steps;
     vector<vector<vector<int>>> vst(N, vector<vector<int>>(M, vector<int>(4, -1)));
     for (int i = 0; i < 4; i++) {
         que_pos.push_back((sx * M + sy) * 4 + i);
+        steps.push_back(0);
         vst[sx][sy][i] = 1;
     }
     int front = 0;
 
     while (front < que.size()) {
+        int cur_dir = que[front] % 4;
+        int cur_y = que[front] / 4 % M;
+        int cur_x = que[front] / 4 / M;
+        int cur_steps = steps[front];
 
+        if (cur_x == ex && cur_y = ey) {
+            cout << cur_steps << endl;
+            break;
+        }
+
+        front++;
+
+        for (int d = 0; d < 4; d++) {
+            int next_x = cur_x + dx * next[d][cur_x][cur_y];
+            int next_y = cur_y + dy * next[d][cur_x][cur_y];
+
+            if (vst[next_x][next_y][d] != -1) continue;
+
+            que.push_back((next_x * M + next_x) * 4 + d);
+            steps.push_back(cur_steps + 1);
+        }
     }
 }
