3048184a42ac469e55cab8358d44d44762d061a6
cypress777 <chenypress@gmail.com>
2019-09-21 00:08:13 +0800

    solving 527

--- euler/RandomizedBinarySearch.cpp
+++ euler/RandomizedBinarySearch.cpp
@@ -20,30 +20,19 @@ double B(long long n) {
 }
 
 double R(long long n) {
-    double res = 0;
-    for (long long i = 0; i < n; i++) {
-        double exp = 0;
-        cout << "--- " << i << endl;
-
-        if (i == 0 || i == n - 1) {
-            exp = ((B(n - 1) + 1.0) * double(n - 1.0) + 1.0) / double(n);
-            cout << B(n - 1) << " " << exp << endl;
-        } else {
-            exp = ((B(i) + 1.0) * double(i) + (B(n - 1 - i) + 1.0) * (n - 1 - i) + 1.0) / double(n);
-            cout << B(i) << " " << B(n - 1 - i) << " " << exp << endl;
-        }
-
-        res += exp;
+    double res = 1;
+    for (long long i = 2; i <= n; i++) {
+        res = (double(i * i) - 1) / double(i * i) * res - double(i - 1) * double(i - 1) / (i * i) + 1;
     }
-
-    res /= double(n);
 
     return res;
 }
 
 int main() {
-    cout << std::setprecision(8) << B(6) << endl;
-    cout << R(6) << endl;
+    long long MM = 1e10;
+    double b = B(MM);
+    double r = R(MM);
+    cout << std::setprecision(8) << b << " " << r << " " << r - b << endl;
 
     return 0;
 }
