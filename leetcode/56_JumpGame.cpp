b0f13031b062776ba2530a1d0b6d9e350b3af842
cypress777 <chenypress@gmail.com>
2019-11-18 17:57:34 +1100

    solved leetcode

--- leetcode/56_JumpGame.cpp
+++ leetcode/56_JumpGame.cpp
@@ -3,18 +3,16 @@ public:
     bool canJump(vector<int>& nums) {
         int ns = nums.size();
         vector<int> reach(ns, 0);
-        vector<int> reachable(ns, false);
 
         for (int i = 0; i < nums.size(); i++) {
             if (i == 0) {
-                reachable[i] = true;
                 reach[i] = i + nums[i];
             } else {
-                reachable[i] = (reach[i - 1] >= i);
-                if (reachable[i]) reach[i] = max(reach[i - 1], i + nums[i]);
+                if (reach[i - 1] < i) return false;
+                reach[i] = max(reach[i - 1], i + nums[i]);
             }
         }
 
-        return reachable[nums.size() - 1];
+        return true;
     }
 };
