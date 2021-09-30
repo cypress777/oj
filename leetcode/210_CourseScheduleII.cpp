698348b78c2749e1eebc655f27f97aac534251a0
ChenYiping <cyp.ress@outlook.com>
2021-09-30 23:47:59 +1000

    mod leet 210

--- leetcode/210_CourseScheduleII.cpp
+++ leetcode/210_CourseScheduleII.cpp
@@ -1,57 +1,41 @@
+#include<vector>
+#include<queue>
+using namespace std;
+
 class Solution {
 public:
-    int N;
-    vector<vector<int>> orders;
-    vector<int> in_degrees;
-    int edges;
-    
-    vector<int> get_path() {
-        vector<int> path;
-        
-        vector<int> queue;
-        int cur = 0;
-        int del_edges = 0;
-        
-        for (int i = 0; i < N; i++) {
-            if (in_degrees[i] == 0) {
-                queue.push_back(i);
-            }
-        }
-        
-        while(cur < queue.size()) {
-            int node = queue[cur];
-            cur++;
-            path.push_back(node);
-            
-            for (int i = 0; i < orders[node].size(); i++) {
-                int next_node = orders[node][i];
-                in_degrees[next_node]--;
-                
-                if (in_degrees[next_node] == 0) queue.push_back(next_node);
-                
-                del_edges++;
-            }
-        }
-        
-        if (del_edges < edges) return vector<int>{};
-        
-        return path;
-    }
-    
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        N = numCourses;
-        orders = vector<vector<int>>(N, vector<int>{});
-        in_degrees = vector<int>(N, 0);
-        edges = prerequisites.size();
-        
-        for (int i = 0; i < prerequisites.size(); i++) {
-            int course = prerequisites[i][0];
-            int pre_course = prerequisites[i][1];
-            
-            orders[pre_course].push_back(course);
-            in_degrees[course]++;
+        vector<vector<int>> graph(numCourses);
+        vector<int> indegrees(numCourses, 0);
+
+        for (auto order: prerequisites) {
+            graph[order[1]].push_back(order[0]);
+            indegrees[order[0]]++;
         }
+
+        queue<int> que;
         
-        return get_path();
+        for (int i = 0; i < numCourses; i++) {
+            if (indegrees[i] == 0) {
+                que.push(i);
+            }
+        }
+
+        auto result = vector<int>{};
+
+        while (!que.empty()) {
+            int cur = que.front();
+            que.pop();
+
+            result.push_back(cur);
+
+            for (int nei : graph[cur]) {
+                indegrees[nei]--;
+                if (indegrees[nei]==0) que.push(nei);
+            }
+        }
+
+        if (result.size() != numCourses) return vector<int>{};
+        return result;
     }
 };
